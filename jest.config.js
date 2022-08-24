3f728297de43b46d84157416c06d92be321d53a9
Felipe Souza <fgsouza93@gmail.com>
2022-08-24 20:04:40 -0300

    chore: configure jest to transform svg

--- jest.config.js
+++ jest.config.js
@@ -3,28 +3,38 @@ module.exports = {
   setupFilesAfterEnv: ['./jest.setup.js'],
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: [
-    "/node_modules/",
-    "/.next/",
-    "/cypress/"
+    '/node_modules/',
+    'src/stories',
+    '/.next/',
+    '/cypress/',
   ],
   moduleNameMapper: {
-    "^components(.*)$": "<rootDir>/components$1",
-    "^pages(.*)$": "<rootDir>/pages$1",
-    "^tests(.*)$": "<rootDir>/tests$1",
+    '^@components(.*)$': '<rootDir>/src/components$1',
+    '^@utils(.*)$': '<rootDir>/src/utils$1',
+    '^pages(.*)$': '<rootDir>/pages$1',
+    '^tests(.*)$': '<rootDir>/tests$1',
+    '^styles(.*)$': '<rootDir>/styles$1',
+    "^.+\\.svg$": "jest-svg-transformer",
   },
-  "collectCoverage": false,
-  "collectCoverageFrom": [
-    "pages/**",
-    "src/**",
+  collectCoverage: false,
+  collectCoverageFrom: [
+    'pages/**',
+    'src/**/*.ts',
+    'src/**/*.tsx',
     //ignored files
-    "!pages/_app.tsx",
+    '!pages/_app.tsx',
+    '!pages/_document.tsx',
+    '!src/stories/**',
+    '!src/**/*.types.ts*',
+    '!src/**/*.stories.ts*',
+    '!src/**/*.style.ts*',
   ],
-  "coverageThreshold": {
-    "global": {
-      "branches": 90,
-      "functions": 90,
-      "lines": 90,
-      "statements": 90,
-    }
-  }
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
 };
